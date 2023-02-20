2462ab8ca882dd43ec7a6aecb5e7cde95763b9aa
Nothando13 <nphumelele972@gmail.com>
2023-02-20 06:55:13 -0800

    Task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
- * Return: Always 0 (Success)
+ * main - a simple program that outputs 0-9 separated by commas
+ *
+ * Return: 0 on success
  */
 int main(void)
 {
-	int n,m,l;
+	int i;
+	int j;
+	int k;
 
-	for(n = 48,n <58,n++)
+	for (i = 48; i < 56; i++)
 	{
-	    for(m = 49;m < 58;m++)
-	      {
-		 for(l = 50, l < 58,l++)
-		{ 
-		if(l > m and m > n)
-	        { 
-		putchar(n);
-		putchar(m);
-		putchar(l);
-		if(n != 55|| m !=56)
+		for (j = i + 1; j < 57; j++)
 		{
-		putchar(',');
-		putchar('');
-	       }
-	     }
-	   }
-	 }
-       } 		
+			for (k = j + 1; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
