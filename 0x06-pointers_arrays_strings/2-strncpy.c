56aa59442df1222632395fc1faa3eaa85ace07f7
Nothando13 <nphumelele972@gmail.com>
2023-03-01 01:42:26 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- **_strncpy -main entry.
- *void: nothing
- *@dest: char pointer
- *@src: char pointer
- *@n: integer variable
- *Description: function that copies a string.
+ * _strncpy -main entry.
+ * void: nothing
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: integer variable
+ * Description: function that copies a string.
  * Return:  pointer to the resulting string dest
- **/
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
