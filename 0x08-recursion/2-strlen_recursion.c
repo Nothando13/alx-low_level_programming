fae471bfc18d334971e8433e7640ab920d6f8495
Nothando13 <nphumelele972@gmail.com>
2023-03-08 00:57:34 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,23 +1,14 @@
-/*
- * File: 2-strlen_recursion.c
- * Auth: smone
- */
-
 #include "main.h"
-
 /**
- * _strlen_recursion - Return the lengt of a string.
- * @s: string to be searched.
+ * _strlen_recursion - find the lengt of a string
+ * @s: string to be searched
  *
- * Return: The length of the string.
+ * Return: string length
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
 
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+	return (0);
 }
