24d5ed6b42d5ec0fc84c1a22585ba5dbfd858c47
Nothando13 <nphumelele972@gmail.com>
2023-03-10 01:08:08 -0800

    0-memset.c

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memset - print uppercase
+ * @s: some character
+ * @b: some char
+ * @n: num
+ *
+ * Return: dont know yet
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+}
